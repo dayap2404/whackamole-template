2378bf70cd2ac9ef2257ef84fe06456f76d23577
Dayani Plasencia <dayaniplasencia@Dayanis-MacBook-Air.local>
2024-11-12 17:28:55 -0500

    new version

--- whackamole.py
+++ whackamole.py
@@ -1,23 +1,58 @@
 import pygame
-
-#helloooo
-#hellohello
+import random
 
 # new comment after token initializaton
 def main():
     try:
         pygame.init()
         # You can draw the mole with this snippet:
-        # screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
+        #screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
         mole_image = pygame.image.load("mole.png")
         screen = pygame.display.set_mode((640, 512))
         clock = pygame.time.Clock()
+        x, y = 0, 0  # top left, starting mole pos
+
         running = True
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    a, b = event.pos
+                    row = a // 32
+                    col = b // 32
+
+                    # if they clicked correctly
+                    if (x//32,y//32) == (row, col):
+                        # put new mole in random place
+                        x = random.randrange(0,19) * 32
+                        y = random.randrange(0,15) * 32
+
+            # reset the grid and screen
             screen.fill("light green")
+            # this is to create the grid
+
+            # draw horizontal lines
+            for i in range(1, 16):
+                pygame.draw.line(
+                    screen,  # where are drawing the lines
+                    (0, 0, 0),  # what color
+                    (0, i * 32),  # whats the starting point
+                    (640, i * 32)
+                )
+
+            # DRAW VERTICAL LINE
+            for i in range(1, 20):  # same thing
+                pygame.draw.line(
+                    screen,
+                    (0, 0, 0),
+                    (i * 32, 0),
+                    (i * 32, 512)
+                )
+            # draw the mole at new position
+            screen.blit(mole_image, (x,y))
             pygame.display.flip()
             clock.tick(60)
     finally:
